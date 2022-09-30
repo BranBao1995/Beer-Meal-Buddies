40f7a3189dc730ac03fd0e2c3258363ba4ef7523
BranBao1995 <brandon.0702@hotmail.com>
2022-09-30 14:03:18 -0400

    changed home.html to index.html

--- assets/js/Beer.js
+++ assets/js/Beer.js
@@ -1,11 +1,10 @@
-
 let recipeEl = document.querySelector("#recipeSearch");
 let selection = localStorage.getItem("beer");
-let homeButtonEl = document.querySelector(".custom-homebtn")
-let modalEl = document.querySelector("#modalBtn")
-let mP1El = document.querySelector("#mP1")
-let mP2El = document.querySelector("#mP2")
-let mP3El = document.querySelector("#mP3")
+let homeButtonEl = document.querySelector(".custom-homebtn");
+let modalEl = document.querySelector("#modalBtn");
+let mP1El = document.querySelector("#mP1");
+let mP2El = document.querySelector("#mP2");
+let mP3El = document.querySelector("#mP3");
 
 //Beer data is fetched from Punkapi based of selecetion from home page
 fetch("https://api.punkapi.com/v2/beers/" + selection)
@@ -29,64 +28,64 @@ function displayBeer(data) {
   const mealPairing3 = data[0].food_pairing[2];
   const abv = data[0].abv;
 
-  document.querySelector("#custom-beername").innerText = name
-  document.querySelector("#tagline").innerText = tagline
-  document.querySelector("#image").src = image
-  document.querySelector("#description").innerText = description
-  document.querySelector("#firstBrewed").innerText = "First brewed in: " + firstBrewed
-  document.querySelector("#mP1").innerText = mealPairing1
-  document.querySelector("#mP2").innerText = mealPairing2
-  document.querySelector("#mP3").innerText = mealPairing3
-  document.querySelector("#abv").innerText = "Alcohol Percentage: " + abv
+  document.querySelector("#custom-beername").innerText = name;
+  document.querySelector("#tagline").innerText = tagline;
+  document.querySelector("#image").src = image;
+  document.querySelector("#description").innerText = description;
+  document.querySelector("#firstBrewed").innerText =
+    "First brewed in: " + firstBrewed;
+  document.querySelector("#mP1").innerText = mealPairing1;
+  document.querySelector("#mP2").innerText = mealPairing2;
+  document.querySelector("#mP3").innerText = mealPairing3;
+  document.querySelector("#abv").innerText = "Alcohol Percentage: " + abv;
 }
 
 //created event listener for search bar with pushed the seach input to our recipe api
 recipeEl.addEventListener("submit", function (event) {
   event.preventDefault();
   let recipe = document.querySelector("#recipeInput").value;
-  if (recipe === ""){
-    $('.tiny.modal')
-  .modal('show')
+  if (recipe === "") {
+    $(".tiny.modal").modal("show");
   } else {
-  let recipeArr = recipe.split(" ");
-  let q = recipeArr.join("-");
-  let queryString = "./Recipe.html?q=" + q;
-  location.assign(queryString);}
+    let recipeArr = recipe.split(" ");
+    let q = recipeArr.join("-");
+    let queryString = "./Recipe.html?q=" + q;
+    location.assign(queryString);
+  }
 });
 
 //created an event listener to go back to home page
-homeButtonEl.addEventListener("click", function() {
-  homePage = "./Home.html";
+homeButtonEl.addEventListener("click", function () {
+  homePage = "./index.html";
   location.assign(homePage);
-})
+});
 
-//added event listener to hide the modal 
-modalEl.addEventListener("click", function (){
-  $('.tiny.modal')
-  .modal('hide');
-})
+//added event listener to hide the modal
+modalEl.addEventListener("click", function () {
+  $(".tiny.modal").modal("hide");
+});
 
 //added event listener to each meal pairing option
-mP1El.addEventListener("click", function(){
-  let recipe = document.querySelector("#mP1").textContent
-  let recipeArr = recipe.split(" ");
-  let q = recipeArr.join("-");
-  let queryString = "./Recipe.html?q=" + q
-  location.assign(queryString);
-})
-
-mP2El.addEventListener("click", function(){
-  let recipe = document.querySelector("#mP2").textContent
+mP1El.addEventListener("click", function () {
+  let recipe = document.querySelector("#mP1").textContent;
   let recipeArr = recipe.split(" ");
   let q = recipeArr.join("-");
   let queryString = "./Recipe.html?q=" + q;
   location.assign(queryString);
-})
+});
 
-mP3El.addEventListener("click", function(){
-  let recipe = document.querySelector("#mP3").textContent
+mP2El.addEventListener("click", function () {
+  let recipe = document.querySelector("#mP2").textContent;
   let recipeArr = recipe.split(" ");
   let q = recipeArr.join("-");
   let queryString = "./Recipe.html?q=" + q;
   location.assign(queryString);
-})
+});
+
+mP3El.addEventListener("click", function () {
+  let recipe = document.querySelector("#mP3").textContent;
+  let recipeArr = recipe.split(" ");
+  let q = recipeArr.join("-");
+  let queryString = "./Recipe.html?q=" + q;
+  location.assign(queryString);
+});
